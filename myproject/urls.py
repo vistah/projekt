bd820d0fbca4ef21895757dbb45ac5dec81fcbb9
Viktoria <stahlbaum.v@gmail.com>
2020-06-18 14:22:42 +0200

    altered some urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('todo.urls'))
+    path('', include('todo.urls')),
+    path('admin/', admin.site.urls)
 ]
