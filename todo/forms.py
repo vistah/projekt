3426865d6f556bce5a24c339851a98d7cd2cd86a
Viktoria <stahlbaum.v@gmail.com>
2020-06-19 08:24:48 +0200

    allow blank spaces

--- todo/forms.py
+++ todo/forms.py
@@ -5,16 +5,16 @@ class TodoForm(forms.Form):
     text = forms.CharField(max_length=40,
                            widget=forms.TextInput(
                                attrs={'class': 'form-control', 'placeholder': ' Your Class', 'aria-label': 'Todo',
-                                      'aria-describedby': 'add-btn', 'pattern': '[A-Za-z ]+', 'autocomplete': 'off'})
+                                      'aria-describedby': 'add-btn', 'autocomplete': 'off'})
                            )
 
 
-# 'pattern':'[A-Za-z ]+' means that there are only letters allowed in the CharField, no digits, no special characters, no umlauts/vowels
+# 'pattern':'[A-Za-z1-3]+' means that there are only letters and digits from 1-3 allowed in the CharField, no special characters, no umlauts/vowels
 
 
 class TodoFormSecond(forms.Form):
     text = forms.CharField(max_length=40,
                            widget=forms.TextInput(
                                attrs={'class': 'form-control', 'placeholder': ' Your Class', 'aria-label': 'Todo',
-                                      'aria-describedby': 'add-btn', 'pattern': '[A-Za-z ]+', 'autocomplete': 'off'})
+                                      'aria-describedby': 'add-btn', 'autocomplete': 'off'})
                            )
