788daf85fc8103b5c85220642841f4774345784c
Viktoria <stahlbaum.v@gmail.com>
2020-07-03 14:14:28 +0200

    Ohne Semester

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=40)
     complete = models.BooleanField(default=False)
-    semester = models.IntegerField()
 
     def __str__(self):
         return self.text
